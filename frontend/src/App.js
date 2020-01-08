27248c475fb9057182f6b7d2d7e6ee446f0ab95f
AlexHuliai <58236141+AlexHuliai@users.noreply.github.com>
2020-01-08 16:17:45 -0600

    todo list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
+
+import Todos from './components/Todos';
+import React, { Component } from 'react';
 
 import './App.css';
+import Header from './components/layout/Header';
+import Add from './components/Add';
 
-function App() {
+class App extends Component {
+  state = {
+    todos : [
+      {
+        id:1,
+        title : 'Take out trash',
+        completed : false 
+      },
+      {
+        id:2,
+        title : 'dinner with wife',
+        completed : true 
+      },
+      {
+        id:3,
+        title : 'meeting with boss',
+        completed : false 
+      },
+    ]
+  }
+  markComplete = (id) =>{
+    this.setState({todos: this.state.todos.map(todo =>{
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })});
+  }
+  deleteTodo=(id) =>{
+    this.setState({todos : [...this.state.todos.filter(todo =>todo.id !== id)]});
+  }
+  render(){
+  console.log(this.state.todos)
   return (
     <div className="App">
-      <h1>Hello</h1>
+     <div className="container">
+      <Header />
+      <Add />
+      <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo ={this.deleteTodo}/>
+      </div>
     </div>
   );
 }
-
+}
 export default App;
